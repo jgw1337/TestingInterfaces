f551680efcfc43b42d5484b34e39b4060c8ceb40
jgwang-github <jgwang.github@gmail.com>
2015-04-05 23:22:35 -0700

    Correct typo when displaying top card

--- src/App.java
+++ src/App.java
@@ -10,11 +10,13 @@ public class App {
 		ArrayList<Card> pokerCards = pokerDeck.getCards();
 		StringBuilder topCard = new StringBuilder();
 		topCard.append("Top card is\t: %s of %ss\n");
+		
 		System.out.printf(topCard.toString(), pokerCards.get(0).getName(),
-				pokerCards.get(1).getSuit());
+				pokerCards.get(0).getSuit());
+		
 		pokerDeck.shuffle();
 		System.out.printf(topCard.toString(), pokerCards.get(0).getName(),
-				pokerCards.get(1).getSuit());
+				pokerCards.get(0).getSuit());
 		pokerCards.get(0).showInfo();
 	}
 
